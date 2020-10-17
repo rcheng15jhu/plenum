68e72d614c49c9741c100969d9256068cf4caaf9
shintyl <tylerjshin@gmail.com>
2020-10-17 02:07:22 +0000

    add refresh instead of redirect

--- homeworks/hw5/src/main/resources/public/js/addauthor.js
+++ homeworks/hw5/src/main/resources/public/js/addauthor.js
@@ -1,11 +1,9 @@
-function addAuthor(name, nationality, numOfBooks) {
-    fetch('http://localhost:7000/addauthor?name=' + name + '&nationality=' + nationality + '&numOfBooks=' + numOfBooks, {
+document.getElementById("submit-author").onclick = function(e) {
+    fetch('http://localhost:7000/addauthor?' +
+        'name=' + document.getElementById("name").value + 
+        '&nationality=' + document.getElementById("nationality").value + 
+        '&numOfBooks=' + document.getElementById("numOfBooks").value, {
             method: 'POST',
         }
     ).then(res => window.location.reload(true));
 }
-
-let addButton = document.querySelectorAll("li.book > button")
-Array.prototype.forEach.call(delButtons, function(button) {
-    button.addEventListener('click', deleteBook.bind(null, button.className));
-});
