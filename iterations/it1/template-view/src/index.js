44a4ba0a53449a8008b5ab03b2d627bc42d61c60
Ricky Cheng <rcheng15@jh.edu>
2020-10-20 19:47:07 -0400

    Refactored React components into different files

--- iterations/it1/template-view/src/index.js
+++ iterations/it1/template-view/src/index.js
@@ -1,74 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-
-const Calendar = (props) => {
-  let template = props.file
-
-  let calendar = new Array(12)
-
-  for (let i = 0; i < calendar.length; i++) {
-    calendar[i] = new Array(7)
-  }
-
-  for (let i = 0; i < 12; i++) {
-    for (let j = 0; j < 7; j++) {
-      calendar[i][j] = 'A'
-    }
-  }
-
-  for (let i = 0; i < template.length; i++) {
-    for (let j = 0; j < template[i].times.length; j++) {
-      calendar[template[i].times[j]][template[i].date] = 'U'
-    }
-  }
-
-  const time = (val) => {
-    if (val === 0 || val === 6) {
-      return 12
-    }
-    return (val * 2) % 12
-  }
-
-  console.log(calendar)
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Su</th>
-          <th>M</th>
-          <th>T</th>
-          <th>W</th>
-          <th>Th</th>
-          <th>F</th>
-          <th>S</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          calendar.map((keyList, i) => (
-            <tr key={i}>
-              <td style={{textAlign:'right'}}>{time(i)}</td>
-              {
-                keyList.map((key, j) =>
-                  <Cell key={j} unavailable={key} />
-                )
-              }
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
-  )
-}
-
-const Cell = (props) => {
-  let styles = { border: '1px solid black', width: '25px', height: '25px', textAlign: 'center' }
-  if (props.unavailable === 'U') {
-    styles.backgroundColor = 'red'
-  }
-  return <td key={props.key} style={styles}></td>
-}
+import Calendar from "./components/Calendar";
 
 const App = () => {
 
@@ -85,7 +17,7 @@ const App = () => {
 
   return (
   <div>
-    <input type='file' onChange={handleChange}></input>
+    <input type='file' onChange={handleChange}/>
     <Calendar file={data}/>
   </div>
   )
