da93a35f972f392bb5bab24109b275f0d68515ac
Ricky Cheng <rcheng15@jh.edu>
2020-10-20 22:04:05 -0400

    waa

---
+++ iterations/it1/template-view/src/pages/makecalendar.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import Calendar from "../components/Calendar";
+
+const App = () => {
+
+    return (
+        <div>
+            <Calendar />
+        </div>
+    )
+};
+
+export default App
