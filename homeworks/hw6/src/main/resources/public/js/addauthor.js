c43b65e32aa0228860c214a3e0e11b7e5ad6b96e
shintyl <tylerjshin@gmail.com>
2020-10-25 04:21:39 +0000

    remove localhostt:7000 in js

--- homeworks/hw6/src/main/resources/public/js/addauthor.js
+++ homeworks/hw6/src/main/resources/public/js/addauthor.js
@@ -1,5 +1,5 @@
 document.getElementById("submit-author").onclick = function(e) {
-    fetch('http://localhost:7000/addauthor?' +
+    fetch('/addauthor?' +
         'name=' + document.getElementById("name").value + 
         '&nationality=' + document.getElementById("nationality").value + 
         '&numOfBooks=' + document.getElementById("numOfBooks").value, {
