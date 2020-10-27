3f6ea655524e59feb6f53b66ae13527743c89d94
Ricky Cheng <rcheng15@jh.edu>
2020-10-27 00:27:36 -0400

    Added necessary ids to templates

--- homeworks/hw6/src/main/resources/public/js/addauthor.js
+++ homeworks/hw6/src/main/resources/public/js/addauthor.js
@@ -3,9 +3,10 @@ document.getElementById("submit-author").onclick = function(e) {
         fetch('/addauthor?' +
         'name=' + document.getElementById("name").value + 
         '&nationality=' + document.getElementById("nationality").value + 
-        '&numOfBooks=' + document.getElementById("numOfBooks").value, {
-            method: 'POST',
-        }
+        '&numOfBooks=' + document.getElementById("numOfBooks").value,
+            {
+                method: 'POST',
+            }
     ).then(res => window.location.reload(true));
     }
 }
