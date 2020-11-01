e24cbad7ffcf7a8bb35460c9f67f44985fbaa071
shintyl <tylerjshin@gmail.com>
2020-11-01 05:57:09 +0000

    fix webpack

--- iterations/src/main/js/src/components/aggregate-calendar.js
+++ iterations/src/main/js/src/components/aggregate-calendar.js
@@ -3,8 +3,53 @@ import React from "react";
 import Cell from './cell'
 import EditableCell from './editable-cell'
 
+let sample_data = {
+    "01": [
+        {
+            "date": 1,
+            "times": [0, 1, 2, 3]
+        },
+        {
+            "date": 4,
+            "times": [5, 7, 8, 10]
+        },
+        {
+            "date": 5,
+            "times": [5]
+        }
+    ],
+    "02": [
+        {
+            "date": 1,
+            "times": [0, 1, 2, 3]
+        },
+        {
+            "date": 4,
+            "times": [5, 7, 8, 10]
+        },
+        {
+            "date": 6,
+            "times": [6]
+        }
+    ],
+    "03": [
+        {
+            "date": 1,
+            "times": [0, 1, 2, 3]
+        },
+        {
+            "date": 4,
+            "times": [5, 7, 8, 10]
+        },
+        {
+            "date": 0,
+            "times": [0]
+        }
+    ]
+}
+
 const aggregate_calendar = (props) => {
-    let template = props.file
+    // let template = props.file
 
     let calendar = new Array(12)
 
@@ -14,18 +59,28 @@ const aggregate_calendar = (props) => {
 
     for (let i = 0; i < 12; i++) {
         for (let j = 0; j < 7; j++) {
-            calendar[i][j] = 'U'
+            calendar[i][j] = 0
         }
     }
 
-    if(template !== undefined && template.dates !== undefined) {
-        console.log("test")
-        let dates = template.dates;
-        for (let i = 0; i < dates.length; i++) {
-            for (let j = 0; j < dates[i].times.length; j++) {
-                calendar[dates[i].times[j]][dates[i].date] = 'A'
-            }
-        }
+    // if (template !== undefined && template.dates !== undefined) {
+    //     console.log("test")
+    //     let dates = template.dates;
+    //     for (let i = 0; i < dates.length; i++) {
+    //         for (let j = 0; j < dates[i].times.length; j++) {
+    //             calendar[dates[i].times[j]][dates[i].date] = 'A'
+    //         }
+    //     }
+    // }
+
+    let users_in_event = Object.keys(sample_data).length
+    console.log(users_in_event)
+    for (const i in sample_data) {
+        console.log(sample_data[i])
+        sample_data[i].forEach(element => {
+            console.log(element.date, element.times)
+            element.times.forEach(time => calendar[time][element.date] = calendar[time][element.date] + 1)
+        })
     }
 
     const time = (val) => {
@@ -35,39 +90,31 @@ const aggregate_calendar = (props) => {
         return (val * 2) % 12
     }
 
-    console.log(calendar)
     return (
         <table>
             <thead>
-            <tr>
-                <th/>
-                <th>Su</th>
-                <th>M</th>
-                <th>T</th>
-                <th>W</th>
-                <th>Th</th>
-                <th>F</th>
-                <th>S</th>
-            </tr>
+                <tr>
+                    <th />
+                    <th>Su</th>
+                    <th>M</th>
+                    <th>T</th>
+                    <th>W</th>
+                    <th>Th</th>
+                    <th>F</th>
+                    <th>S</th>
+                </tr>
             </thead>
             <tbody>
-            {
-                calendar.map((keyList, i) => (
-                    <tr key={i}>
-                        <td style={{textAlign: 'right'}}>{time(i)}</td>
-                        {(() => {
-                            if(props.editable !== undefined && props.editable === true)
-                                return keyList.map((key, j) =>
-                                    <EditableCell key={j} unavailable={key} time={i} day={j}/>
-                                )
-                            else
-                                return keyList.map((key, j) =>
-                                    <Cell key={j} unavailable={key} time={i} day={j}/>
-                                )
-                        })()}
-                    </tr>
-                ))
-            }
+                {
+                    calendar.map((keyList, i) => (
+                        <tr key={i}>
+                            <td style={{ textAlign: 'right' }}>{time(i)}</td>
+                            {keyList.map((key, j) =>
+                                <td>key={j}{calendar[i][j]}</td> 
+                            )}
+                        </tr>
+                    ))
+                }
             </tbody>
         </table>
     )
--- iterations/src/main/js/webpack.config.js
+++ iterations/src/main/js/webpack.config.js
@@ -2,54 +2,69 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fs = require('fs')
 
-let pages = fs.readdirSync('src/pages').map(i => i.replace('.js', ''))
+module.exports = (env, options) => {
+    let pages = fs.readdirSync('src/pages').map(i => i.replace('.js', ''))
 
-let entryGenerator = {}
-pages.forEach(page => entryGenerator[page] = './src/pages/' + page + '.js')
+    let entryGenerator = {}
+    pages.forEach(page => entryGenerator[page] = './src/pages/' + page + '.js')
 
-let pageGenerator = pages.map(page =>
-    new HtmlWebpackPlugin({
-        title: `${page}`,
-        template: 'template.ejs',
-        inject: true,
-        chunks: [`${page}`],
-        filename: path.resolve(__dirname, '..', `resources/public/static/html/${page}.html`)
-    })
-)
+    let pageGenerator = []
 
-const config = {
-    entry: entryGenerator,
-    output: {
-        path: path.resolve(__dirname, '..', 'resources/public/static'),
-        filename: 'js/[name].js',
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, '..', 'resources/public/static/html'),
-        compress: true,
-        port: 3000,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:7000',
+    if (options.mode === 'development') {
+        pageGenerator = pages.map(page =>
+            new HtmlWebpackPlugin({
+                title: `${page}`,
+                template: 'template.ejs',
+                inject: true,
+                chunks: [`${page}`],
+                filename: path.resolve(__dirname, '..', `resources/public/static/html/${page}.html`)
+            })
+        )
+    } 
+    else {
+        pageGenerator = pages.map(page =>
+            new HtmlWebpackPlugin({
+                title: `${page}`,
+                template: 'template.ejs',
+                inject: true,
+                chunks: [`${page}`],
+                publicPath: 'static',
+                filename: path.resolve(__dirname, '..', `resources/public/static/html/${page}.html`)
+            })
+        )
+    }
+    return {
+        entry: entryGenerator,
+        output: {
+            path: path.resolve(__dirname, '..', 'resources/public/static'),
+            filename: 'js/[name].js',
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, '..', 'resources/public/static/html'),
+            compress: true,
+            port: 3000,
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:7000',
+                }
             }
-        }
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
                 },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    plugins: pageGenerator
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        plugins: pageGenerator
+    }
 }
-
-module.exports = config
