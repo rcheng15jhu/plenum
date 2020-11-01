c3a73b352ce587d4ce642a81a9a082b283faa63c
Ricky Cheng <rcheng15@jh.edu>
2020-11-01 00:54:35 -0400

    Making list-calendar redirect

--- iterations/src/main/js/src/pages/list-calendar.js
+++ iterations/src/main/js/src/pages/list-calendar.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Viewable_list from "../components/viewable-list";
 
-const App = () => (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
+const App = () =>  {
+
+    const [calendars, setCalendars] = useState([])
+
+    useEffect(() => {
+        fetch('/api/calendar', {
+                method: 'GET',
+                mode: 'cors'
+            }
+        ).then(res => {
+            return res.json()
+        }).then(data => {
+            console.log(data)
+            setCalendars([...data])
+        })
+    }, [])
+
+
+    let calendarNames = calendars.map(calendar => {return {id: calendar.id, content: calendar.title}})
+
+    return (
+        <div className="divContents">
+            <Viewable_list list={calendarNames} clicked={updateActive} />
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
