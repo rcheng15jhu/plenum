0e751ad99dcc16b87f3f276f429078084a224403
shintyl <tylerjshin@gmail.com>
2020-11-01 01:49:32 +0000

    port over create calendar

---
+++ iterations/src/main/js/src/components/cell.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+const cell = (props) => {
+    let styles = {border: '1px solid black', width: '25px', height: '25px', textAlign: 'center', outline: 'none'}
+
+    if (props.unavailable !== 'A') {
+        styles.backgroundColor = 'red'
+    }
+    return <td><button style={styles} className={"day" + props.day} value={props.time}/></td>
+};
+
+export default cell
--- iterations/src/main/js/src/pages/create-calendar.js
+++ iterations/src/main/js/src/pages/create-calendar.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 import ReactDOM from 'react-dom'
+import Calendar from "../components/calendar";
+import uploadTemplate from "../services/calendar-manager.js";
 
-const App = () => (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
+const App = () => {
+
+    return (
+        <div>
+            <Calendar editable={true}/>
+            <div className="form-example">
+                <label htmlFor="title">Enter your calendar title: </label>
+                <input type="text" name="title" id="title"/>
+            </div>
+            <button onClick={uploadTemplate} >Upload template!</button>
+        </div>
+    )
+};
 
 ReactDOM.render(<App />, document.getElementById('root'))
