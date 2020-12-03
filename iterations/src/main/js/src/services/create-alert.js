62d952a5420e9431a5b8a481b8cb291459118a85
leezionf <53808407+leezionf@users.noreply.github.com>
2020-12-03 13:56:06 -0500

    Update create-alert.js

--- iterations/src/main/js/src/services/create-alert.js
+++ iterations/src/main/js/src/services/create-alert.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CollapsibleAlert from "../components/collapsibleAlert";
 
+//This is to create alert messages and show its severity.
 export default function createAlert(msg, severity) {
     let temp = document.createElement('alert');
     ReactDOM.render(<CollapsibleAlert severity={severity} msg={msg}></CollapsibleAlert>, temp);
