377c952315f843e487536a2dcf9fc2d8700e3eeb
Everett X <everettxu@Everetts-Air.fios-router.home>
2020-12-06 02:20:14 -0500

    Background image on index page

--- iterations/src/main/js/src/pages/index.js
+++ iterations/src/main/js/src/pages/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom'
 import {Typography} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import {cookieManager, checkCookie} from "../services/cookie-manager";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import theme from "../components/baseline-theme";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+import backgroundPhoto from '../resources/background.jpg';
+
+const useStyles = makeStyles((theme) => ({
+    backgroundDiv: {
+        height: '100%',
+        width: '100%',
+        top: '0px',
+        left:'0px',
+        position: 'fixed',
+        border: '2px solid black',
+        backgroundRepeat: 'no-repeat',
+    },
+}));
 
 const App = () => {
+
+    const classes = useStyles();
 
     const username = cookieManager('username');
 
@@ -17,29 +35,28 @@ const App = () => {
     }
 
     return (
-        <div style={{'marginTop': '60px'}}>
-            <Typography variant='h3' align='center' style={{'margin': '20% 0 30px 0'}}>
-                Welcome to Plenum!
-            </Typography>
-            <div style={{'display': "flex",
-                        'alignItems': "center",
-                        'justifyContent': "center",
-                        'width': '100%',
-            }}>
-                {username != '' ?
-                    <Button variant='contained' color='primary' onClick={handleLogin} style={{'margin': '0 10px'}}>
-                        Login
+        <ThemeProvider theme={theme}>
+            <img src={backgroundPhoto} className={classes.backgroundDiv}/>
+            <div style={{'marginTop': '20%'}}>
+                <Typography variant='h3' align='center' style={{'margin': '20% 0 30px 0'}}>
+                    Welcome to Plenum!
+                </Typography>
+                <div style={{'display': "flex",'alignItems': "center",'justifyContent': "center",'width': '100%',}}>
+                    {username === '' ?
+                        <Button variant='contained' color='primary' onClick={handleLogin} style={{'margin': '0 10px'}}>
+                            Login
+                        </Button>
+                        :
+                        <Button variant='contained' color='inherit' href='list-calendar'>
+                            Your calendar
+                        </Button>
+                    }
+                    <Button variant='contained' color='inherit' href='list-public-events'>
+                        View public events
                     </Button>
-                    :
-                    <Button variant='contained' color='inherit' href='list-calendar'>
-                        Your calendar
-                    </Button>
-                }
-                <Button variant='contained' color='inherit' href='list-public-events'>
-                    View public events
-                </Button>
+                </div>
             </div>
-        </div>
+        </ThemeProvider>
     )
 }
 
