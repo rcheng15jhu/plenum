2a797582245642363f1a675ec5eb017766cb292f
leezionf <slee454@jhu.edu>
2020-12-06 18:26:00 -0500

    edit profile database

--- iterations/src/main/java/model/SqlSchema.java
+++ iterations/src/main/java/model/SqlSchema.java
@@ -6,7 +6,11 @@ public class SqlSchema {
           " id            serial PRIMARY KEY," +
           " name          VARCHAR(100) NOT NULL UNIQUE," +
           " password      VARCHAR(48) NOT NULL," +
-          " salt          VARCHAR(36) NOT NULL" +
+          " salt          VARCHAR(36) NOT NULL," +
+          " email          VARCHAR(100) NOT NULL," +
+          " affil          VARCHAR(200) NOT NULL," +
+          " description   VARCHAR(1000) NOT NULL," +
+          " pic          VARCHAR(100) NOT NULL" +
           ");";
 
   public static String EventsSchema = "CREATE TABLE IF NOT EXISTS Events (" +
