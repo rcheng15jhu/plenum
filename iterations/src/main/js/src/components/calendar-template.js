15637cdff137a719d8ca368048c09afd6b610ddc
Ricky Cheng <rcheng15@jh.edu>
2020-12-06 01:47:11 -0500

    Added updated calendar-template

--- iterations/src/main/js/src/components/calendar-template.js
+++ iterations/src/main/js/src/components/calendar-template.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {getTime} from "../services/calendar-manager";
 
 const calendarTemplate = (props) => (
     <table style={{borderCollapse: 'collapse', margin: '5px 10px 10px 0px'}}>
@@ -15,7 +16,35 @@ const calendarTemplate = (props) => (
             </tr>
         </thead>
         <tbody>
-            {props.children}
+        {props.children.map((row, i) => {
+            let moreStyles = {borderTop: '1px white solid'}
+            if(i % 4 === 0) {
+                moreStyles = {borderTop: '1px black solid'}
+            }
+            else if (i % 2 === 0) {
+                moreStyles = {borderTop: '1px black dotted'}
+            }
+            return (
+                <tr key={i}>
+                    {i % 4 === 0
+                        ? <td style={{textAlign: 'right', verticalAlign: 'top'}} rowSpan={4}>{getTime(i)}</td>
+                        : null
+                    }
+                    {row.map(cell => React.cloneElement(cell, {moreStyles: moreStyles}))}
+                    <td style={{borderLeft: '1px black solid', width: '0px'}} />
+                </tr>
+            )
+        })}
+            <tr>
+                {(() => {
+                    let hi = [<td key={0} />]
+                    hi.push([0, 0, 0, 0, 0, 0, 0].map((trash, i) => (
+                        <td key={i + 1} style={{borderTop: '1px black solid', height: '0px'}}/>
+                    )))
+                    hi.push(<td key={8} />)
+                    return hi
+                })()}
+            </tr>
         </tbody>
     </table>
 )
