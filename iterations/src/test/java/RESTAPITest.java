9ba4ea758ba990e9c2ed88b0ba57869b7ffbcca6
GWCMelody <melody2011102@gmail.com>
2020-12-07 11:58:06 +0800

    added tests

--- iterations/src/test/java/RESTAPITest.java
+++ iterations/src/test/java/RESTAPITest.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
 import okhttp3.*;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.sql2o.Connection;
@@ -8,12 +9,14 @@ import org.sql2o.Sql2o;
 import java.io.IOException;
 import java.sql.*;
 
+import static org.junit.Assert.*;
 
 public class RESTAPITest {
 
     final static String URI = "jdbc:sqlite:./Plenum.db";
     static OkHttpClient client;
     static Gson gson;
+    private static Statement st;
 
     @BeforeClass
     public static void beforeClassTests() throws SQLException {
@@ -39,83 +42,90 @@ public class RESTAPITest {
         gson = new Gson();
     }
 
-    @Test
-    public void testAddUser() throws IOException {
-        // RequestBody postBody = new FormBody.Builder()
-        //         .add("name", "Sadegh Hedayat")
-        //         .add("numOfBooks", "26")
-        //         .add("nationality", "Iranian")
-        //         .build();
-        // Request request = new Request.Builder()
-        //         .url("http://localhost:7000/addauthor")
-        //         .post(postBody)
-        //         .build();
-        // Response response = client.newCall(request).execute();
-        // assertEquals(201, response.code());
+    @Before
+    public void beforeEachTest() throws SQLException {
+        //Create new tables for each test
+        String sql = "DROP TABLE IF EXISTS Authors";
+        st.execute(sql);
+        String sq2 = "DROP TABLE IF EXISTS Books";
+        st.execute(sq2);
+
+        String sql3 = "CREATE TABLE IF NOT EXISTS Authors (id INTEGER PRIMARY KEY, name VARCHAR(100) NOT NULL UNIQUE," +
+                " numOfBooks INTEGER, nationality VARCHAR(30));";
+        st.execute(sql3);
+
+        String sq4 = "CREATE TABLE IF NOT EXISTS Books (" +
+                " id        INTEGER PRIMARY KEY," +
+                " title     VARCHAR(100) NOT NULL," +
+                " isbn      VARCHAR(100) NOT NULL UNIQUE," +
+                " publisher VARCHAR(100)," +
+                " year      INTEGER," +
+                " authorId  INTEGER NOT NULL," +
+                " FOREIGN KEY(authorId)" +
+                " REFERENCES Authors (id)" +
+                "   ON UPDATE CASCADE" +
+                "   ON DELETE CASCADE" +
+                ");";
+        st.execute(sq4);
     }
 
-/*
-+//    @Test
-//    public void testAddUser() throws IOException {
-//         RequestBody postBody = new FormBody.Builder()
-//                 .add("name", "username123")
-//                 .add("password", "123")
-//                 .build();
-//         Request request = new Request.Builder()
-//                 .url("http://localhost:7000/api/adduser")
-//                 .post(postBody)
-//                 .build();
-//         Response response = client.newCall(request).execute();
-//         assertEquals("", response.code());
-//    }
+    @Test
+    public void testAddUser() throws IOException {
+        RequestBody postBody = new FormBody.Builder()
+                .add("name", "username123")
+                .add("password", "123")
+                .build();
+        Request request = new Request.Builder()
+                 .url("http://localhost:7000/api/adduser")
+                 .post(postBody)
+                 .build();
+         Response response = client.newCall(request).execute();
+         assertEquals("", response.code());
+    }
 
-//    @Test
-//    public void testLogin() throws IOException {
-//        RequestBody postBodyAdd = new FormBody.Builder()
-//                .add("name", "username123")
-//                .add("password", "123")
-//                .build();
-//        Request requestAdd = new Request.Builder()
-//                .url("http://localhost:7000/api/adduser")
-//                .post(postBodyAdd)
-//                .build();
-//        client.newCall(requestAdd).execute();
-//
-//        RequestBody postBody = new FormBody.Builder()
-//                .add("name", "username123")
-//                .add("password", "123")
-//                .build();
-//        Request request = new Request.Builder()
-//                .url("http://localhost:7000/api/login")
-//                .post(postBody)
-//                .build();
-//        Response response = client.newCall(request).execute();
-//        assertEquals(201, response.code());
-//
-//        RequestBody postBody1 = new FormBody.Builder()
-//                .add("name", "username12")
-//                .add("password", "123")
-//                .build();
-//        Request request1 = new Request.Builder()
-//                .url("http://localhost:7000/api/login")
-//                .post(postBody1)
-//                .build();
-//        Response response1 = client.newCall(request1).execute();
-//        assertEquals(401, response1.code());
-//
-//        RequestBody postBody2 = new FormBody.Builder()
-//                .add("name", "username123")
-//                .add("password", "12345")
-//                .build();
-//        Request request2 = new Request.Builder()
-//                .url("http://localhost:7000/api/login")
-//                .post(postBody2)
-//                .build();
-//        Response response2 = client.newCall(request2).execute();
-//        assertEquals(401, response2.code());
-//    }
-}
- */
+    @Test
+    public void testLogin() throws IOException {
+        RequestBody postBodyAdd = new FormBody.Builder()
+                .add("name", "username123")
+                .add("password", "123")
+                .build();
+        Request requestAdd = new Request.Builder()
+                .url("http://localhost:7000/api/adduser")
+                .post(postBodyAdd)
+                .build();
+        client.newCall(requestAdd).execute();
 
+        RequestBody postBody = new FormBody.Builder()
+                .add("name", "username123")
+                .add("password", "123")
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:7000/api/login")
+                .post(postBody)
+                .build();
+        Response response = client.newCall(request).execute();
+        assertEquals(201, response.code());
 
+        RequestBody postBody1 = new FormBody.Builder()
+                .add("name", "username12")
+                .add("password", "123")
+                .build();
+        Request request1 = new Request.Builder()
+                .url("http://localhost:7000/api/login")
+                .post(postBody1)
+                .build();
+        Response response1 = client.newCall(request1).execute();
+        assertEquals(401, response1.code());
+
+        RequestBody postBody2 = new FormBody.Builder()
+                .add("name", "username123")
+                .add("password", "12345")
+                .build();
+        Request request2 = new Request.Builder()
+                .url("http://localhost:7000/api/login")
+                .post(postBody2)
+                .build();
+        Response response2 = client.newCall(request2).execute();
+        assertEquals(401, response2.code());
+    }
 }
